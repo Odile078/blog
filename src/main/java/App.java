ad7270db4068aecc6dba9b7d68fe90fe6f8f23ee
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-23 14:04:02 +0200

    Added the project

---
+++ src/main/java/App.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import models.Post;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        port(getHerokuAssignedPort());
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/posts/new", (request, response) -> { //URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
---
+++ src/main/java/models/Post.java
@@ -0,0 +1,47 @@
+package models;
+
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Post {
+    private final String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+    private boolean published;
+    private LocalDateTime createdAt;
+    private int id;
+
+    public Post (String content){
+        this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll(){
+        return instances;
+    }
+
+    public static void clearAllPosts(){
+        instances.clear(); //clear as a method is part of the ArrayList class.
+    }
+
+    public boolean getPublished(){
+        return this.published;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+
+}
